bfe3a7bdc299fe9c57b10bb4d89469348a5fee64
AlterSabeh <betamaximum22@gmail.com>
2024-03-29 00:23:41 +0800

    updated file name

--- rust/src/hello.rs
+++
@@ -1,8 +0,0 @@
-use chrono::{DateTime, Local};
-
-fn main() {
-  let now: DateTime<Local> = Local::now();
-
-  println!("Hello World from Rust !");
-  println!("Current Date and Time: {}", now.format("%Y-%m-%d %H:%M:%S"));
-}
