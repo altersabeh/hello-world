260d8c1d80b72db0b80d6d8fc8a8709472368b99
AlterSabeh <betamaximum22@gmail.com>
2024-03-29 20:18:37 +0800

    fixed formatting

--- cargo/src/hello_world.rs
+++ cargo/src/hello_world.rs
@@ -6,22 +6,22 @@ use chrono::{DateTime, Local};
 use std::env;
 
 fn print_greeting(args: &[String]) {
-  if args.len() > 1 {
-    println!("Hi {}, greetings from Cargo !", args[1..].join(" "));
-  } else {
-    println!("Hello World from Cargo !");
-  }
+    if args.len() > 1 {
+        println!("Hi {}, greetings from Cargo !", args[1..].join(" "));
+    } else {
+        println!("Hello World from Cargo !");
+    }
 }
 
 fn print_date() {
-  let current_date: DateTime<Local> = Local::now();
-  let layout: &str = "%B %e, %Y %H:%M:%S";
-  let formatted_date: String = current_date.format(layout).to_string();
-  println!("Today is {}", formatted_date);
+    let current_date: DateTime<Local> = Local::now();
+    let layout: &str = "%B %e, %Y %H:%M:%S";
+    let formatted_date: String = current_date.format(layout).to_string();
+    println!("Today is {}", formatted_date);
 }
 
 fn main() {
-  let args: Vec<_> = env::args().collect();
-  print_greeting(&args);
-  print_date();
+    let args: Vec<_> = env::args().collect();
+    print_greeting(&args);
+    print_date();
 }
