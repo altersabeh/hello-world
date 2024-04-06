d9bf01b24ff2a7250dc480c44825489d1e1877e0
AlterSabeh <betamaximum22@gmail.com>
2024-04-06 23:20:07 +0800

    Refactor translation maps

--- gradle/subprojects/translator/src/main/kotlin/com/prog/translator/Translator.kt
+++ gradle/subprojects/translator/src/main/kotlin/com/prog/translator/Translator.kt
@@ -3,26 +3,26 @@ package com.prog.translator
 import kotlin.collections.hashMapOf
 
 val helloTranslations =
-        hashMapOf(
-                "French" to "Bonjour",
-                "Spanish" to "Hola",
-                "German" to "Hallo",
-                "Italian" to "Ciao",
-                "Dutch" to "Hallo",
-        )
+    hashMapOf(
+        "French" to "Bonjour",
+        "Spanish" to "Hola",
+        "German" to "Hallo",
+        "Italian" to "Ciao",
+        "Dutch" to "Hallo",
+    )
 
 val greetingsFromTranslations =
-        hashMapOf(
-                "French" to "salutations de",
-                "Spanish" to "saludos desde",
-                "German" to "grüße aus",
-                "Italian" to "saluti da",
-                "Dutch" to "groeten uit",
-        )
+    hashMapOf(
+        "French" to "salutations de",
+        "Spanish" to "saludos desde",
+        "German" to "grüße aus",
+        "Italian" to "saluti da",
+        "Dutch" to "groeten uit",
+    )
 
 fun translate(
-        language: String,
-        name: String,
+    language: String,
+    name: String,
 ): String {
     val greeting = helloTranslations[language]
     val greetingsFrom = greetingsFromTranslations[language]
