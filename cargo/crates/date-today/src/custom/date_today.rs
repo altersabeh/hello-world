a6b3f6e45fc427ed0d10a136799e2495972be8e8
AlterSabeh <betamaximum22@gmail.com>
2024-04-07 19:37:22 +0800

    add documentation comments

--- cargo/crates/date-today/src/custom/date_today.rs
+++ cargo/crates/date-today/src/custom/date_today.rs
@@ -1,5 +1,8 @@
-use cargo_hello::hello_world::print_date;
+use prog::hello_world::print_date;
 
+/// # `main`
+///
+/// This function is the entry point of the program. It prints the current date.
 fn main() {
     println!("{}", print_date());
 }
